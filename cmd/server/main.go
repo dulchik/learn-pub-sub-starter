8593915c4ad32e1b0842616b5f9982adf265c76e
dulchik <bubnyukab@gmail.com>
2025-11-10 15:43:32 +0100

    added an exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/dulchik/learn-pub-sub-starter/internal/routing"
+	"github.com/dulchik/learn-pub-sub-starter/cmd/internal/pubsub"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	const rabbitConnString := "amqp://guest:guest@localhost:5672/"
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
 	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
@@ -18,6 +20,16 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	rabbitChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not get the channel from RabbitMQ: %v", err)
+	}
+
+	err = pubsub.PublishJSON(rabbitChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("could not publish json: %v", err)
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
